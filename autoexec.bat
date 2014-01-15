31e7e4017f4bdb84b20440e6a327755ceeb0e0e8
ya-mouse <mouse@ya.ru>
2014-01-16 01:00:20 +0300

    Remove obsolete stuff

--- autoexec.bat
+++ autoexec.bat
@@ -13,33 +13,9 @@ echo BIOS: %BIOS% Date: %BDATE%|%L%
 echo BOARD: %BOARD1% / %BOARD2%|%L%
 pciscan -x -v a:\utils\ndis.map|%L%
 call net start
-if errorlevel 1 goto exit
+if errorlevel 1 goto error
 
 c:\
-echo.|%L%
-echo Current BIOS version is %BIOS%|%L%
-echo Trying to get files for `%BOARD2%'|%L%
-set BOARD=setup.yandex.net/bios/%BOARD2%
-call unpack .tar.gz
-if not errorlevel 1 goto flash
-echo.|%L%
-echo Trying to get files for `%BOARD1%'|%L%
-set BOARD=setup.yandex.net/bios/%BOARD1%
-call unpack .tar.gz
-if errorlevel 1 goto boarderror
-set BOARD1=
-set BOARD2=
-:flash
-if exist c:\flash.bat c:\flash.bat
-echo.
-echo FATAL: c:\flash.bat not exists!!!|%L%
-echo.
-goto exit
-
-:boarderror
-echo.
-echo FATAL: Out of Luck! Board %BOARD1% / %BOARD2% not supported.|%L%
-echo.
 goto exit
 
 :error
